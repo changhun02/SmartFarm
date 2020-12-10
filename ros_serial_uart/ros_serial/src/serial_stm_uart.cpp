c232876b2b3350a2ae1f0f3947e61432693a8dfc
CH_Lee <changhun02@github.com>
2020-12-10 16:38:09 +0900

    Passing

--- ros_serial_uart/ros_serial/src/serial_stm_uart.cpp
+++ ros_serial_uart/ros_serial/src/serial_stm_uart.cpp
@@ -1,6 +1,6 @@
 // By serial uart comm, read Bluetooth RSSI data & Sonar array data
 
-#include "ros/ros.h"
+#include <ros/ros.h>
 #include "std_msgs/String.h"
 #include "std_msgs/ByteMultiArray.h"
 #include "std_msgs/Empty.h"
@@ -12,8 +12,45 @@
 #include "math.h"
 #include "stdio.h"
 
+#define COM_STATE_CONNECTION    1
+#define COM_STATE_UNCONNECTION  0
+#define BUF_SIZE                11
+#define RSSI_BUF_SIZE           11
+#define SONAR_BUF_SIZE          11
 serial::Serial ser;
 
+unsigned int start_bit_1 = 0xF0, start_bit_2 = 0xF1;
+unsigned int Sonar_buf[SONAR_BUF_SIZE];
+unsigned char RSSI[RSSI_BUF_SIZE];
+uint8_t buf[BUF_SIZE];
+
+// Check if Serial Communication is Successful
+bool Serial_check(uint8_t *a){
+  unsigned int sum = 0;
+  if(a[0] == start_bit_1){
+    if(a[1] == start_bit_2){
+      if(a[2] == COM_STATE_CONNECTION){
+        for(int i = 3 ; i <= 8 ; i++){
+          sum += a[i];
+        }
+
+        if(sum == a[9])
+          return true;
+        else
+          return false;
+      }
+      else{
+        return false;
+      }
+    }
+    else{
+      return false;
+    }
+  }
+  else {
+    return false;
+  }
+}
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "serial_stm_uart");
@@ -46,8 +83,32 @@ int main(int argc, char **argv)
   }
 
   ros::Rate loop_rate(20);
+
   while(ros::ok()){
     ros::spinOnce();
+    // Read data from stm32(RSSI, Sonar)
+    ser.read(buf, BUF_SIZE);
+
+    // if Serial is Successful
+    if(Serial_check(buf)){
+      int cnt = 0;
+
+      // RSSI data Passing(ASCII)
+      for(int i = 3 ; i <= 6 ; i++){
+        RSSI[cnt] = (unsigned char)buf[i];
+        cnt ++;
+      }
+
+      cnt = 0;
+
+      // Sonar data Passing(unsigned int)
+      for(int j = 7 ; j <= 9 ; j++){
+        Sonar_buf[cnt] = buf[j];
+        cnt ++;
+      }
+
+    }
+
     loop_rate.sleep();
   }
 }
